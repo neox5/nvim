c6c7b35756fb1a4ea763a8751e5f81c342465b41
neox5 <neox5@orion.localdomain>
2024-11-13 07:43:39 +0100

    adds gopls config for auto-format on save

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,18 +1,56 @@
 return {
   "neovim/nvim-lspconfig",
-  dependencies = { "hrsh7th/cmp-nvim-lsp" },
+  dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/nvim-cmp" },
   config = function()
+    lspconfig = require("lspconfig")
+    
     -- Reserve a space in the gutter
     vim.opt.signcolumn = "yes"
 
     -- Add cmp_nvim_lsp capabilities settings to lspconfig
     -- This should be executed before you configure any language server
-    local lspconfig_defaults = require("lspconfig").util.default_config
+    local lspconfig_defaults = lspconfig.util.default_config
     lspconfig_defaults.capabilities = vim.tbl_deep_extend(
       "force",
       lspconfig_defaults.capabilities,
       require("cmp_nvim_lsp").default_capabilities()
     )
+
+    -- This is where you enable features that only work
+    -- if there is a language server active in the file
+    vim.api.nvim_create_autocmd("LspAttach", {
+      desc = "LSP actions",
+      callback = function(event)
+        local opts = {buffer = event.buf}
+
+        vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+        vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+        vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+        vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+        vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+        vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+        vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+        vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+        vim.keymap.set({"n", "x"}, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+        vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+      end,
+    })
+
+    -- golsp
+    -- > auto-format on save
+    lspconfig.gopls.setup({
+      on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("GoFormat", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end
+          })
+        end 
+      end
+    })
 
   end
 }
@@ -21,7 +59,7 @@ return {
 --   "neovim/nvim-lspconfig",
 --   config = function()
 --     -- General LSP settings can be configured here
---     local lspconfig = require('lspconfig')
+--     local lspconfig = require("lspconfig")
 --
 --     -- Setup other language servers if needed
 --     -- lspconfig.pyright.setup{}
