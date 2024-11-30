28a238bd9baa3a5a5fa15409b344b398e2f23134
neox5 <neox5@orion.localdomain>
2024-11-30 21:51:50 +0100

    adds lualine.lua plugin

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,50 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup {
+      options = {
+        theme = "rose-pine",
+        component_separators = { left = "·", right = "·" },
+        section_separators = { left = "", right = "" },
+        globalstatus = true,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { 
+          {
+            "branch",
+            icon = "",
+          }
+        },
+        lualine_c = {
+          {
+            "diff",
+            symbols = {
+              added = " ",
+              modified = "󰏫 ",
+              removed = " "
+            },
+          }
+        },
+        lualine_x = {
+          {
+            "diagnostics",
+            sources = { "nvim_diagnostic" },
+            symbols = { error = " ", warn = " ", info = " ", hint = "󰌶 " },
+          }
+        },
+        lualine_y = { "filetype" },
+        lualine_z = { "location" }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {}
+      },
+    }
+  end
+}
