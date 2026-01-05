f1eaf3d669ca3cb029534e0182447bf57703c79f
neox5 <faustmannchr@gmail.com>
2026-01-05 12:36:01 +0100

    fix lsp folder structure

--- lua/utils/functions.lua
+++ lua/utils/functions.lua
@@ -2,22 +2,22 @@ local M = {}
 
 -- Uninstall function to remove installed plugins
 function M.uninstall_plugins()
-  local data_path = vim.fn.stdpath("data")
-  local lazy_path = data_path .. "/lazy"
-  
-  -- Confirmation prompt
-  local confirm = vim.fn.confirm("Are you sure you want to uninstall all plugins?", "&Yes\n&No", 2)
-  if confirm ~= 1 then
-    print("Uninstall canceled.")
-    return
-  end
+	local data_path = vim.fn.stdpath("data")
+	local lazy_path = data_path .. "/lazy"
 
-  -- Delete the lazy.nvim plugin directory
-  vim.fn.delete(lazy_path, "rf")
-  print("All plugins have been uninstalled.")
+	-- Confirmation prompt
+	local confirm = vim.fn.confirm("Are you sure you want to uninstall all plugins?", "&Yes\n&No", 2)
+	if confirm ~= 1 then
+		print("Uninstall canceled.")
+		return
+	end
 
-  -- Optionally, remove any compiled files or cache
-  -- vim.fn.delete(data_path .. '/plugin', 'rf')
+	-- Delete the lazy.nvim plugin directory
+	vim.fn.delete(lazy_path, "rf")
+	print("All plugins have been uninstalled.")
+
+	-- Optionally, remove any compiled files or cache
+	-- vim.fn.delete(data_path .. '/plugin', 'rf')
 end
 
 return M
